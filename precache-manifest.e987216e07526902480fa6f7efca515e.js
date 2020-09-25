a18105c4c7019d5ff47865787f932ffabd83b9cc
sumancfc <sumanstha999@gmail.com>
2020-09-25 17:32:41 +0545

    Updates

---
+++ precache-manifest.e987216e07526902480fa6f7efca515e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869ce4d72dc47f127ae358bdba3bad6b",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "baa833480643d37e9201",
+    "url": "/netflix-clone/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "baa833480643d37e9201",
+    "url": "/netflix-clone/static/js/2.dcbf5b68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix-clone/static/js/2.dcbf5b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdad5995c76e9d7a81d9",
+    "url": "/netflix-clone/static/js/main.77299dc4.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
